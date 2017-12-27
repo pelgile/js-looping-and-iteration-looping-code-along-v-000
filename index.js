1a34f1d34a9c3931904492f75ca2b800b3808454
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-27 04:12:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,3 @@ function printBadges(array){
 }
 
 
-function tailsNeverFails(){
-  counter = 0;
-  while (Math.random() <=0.5) {
-    counter = counter + 1
-  }
-  message = `You got ${counter} tails in a row!`
-  return message
-}
