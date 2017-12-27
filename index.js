b6fcab474d60d3aa9c74d1242ecb244e779eba9e
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-27 04:33:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function printBadges(array){
 
 function tailsNeverFails(){
   let counter = 0
-  while( Math.random >= 0.5){
+  while( Math.random <= 0.5){
     counter = counter + 1;
   }
   let message = `You got ${counter} tails in a row!`
